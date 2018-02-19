f3a985722c89d0230f7e44efbe5b01385e2febd4
HardRic501 <hardric501@gmail.com>
2018-02-19 15:26:48 +0100

    yolóka

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from pygame.locals import *
 
 pygame.init()
 width, height = 640, 480
-screen=pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode((width, height))
 
-#ÁPDÉT
-#  Gameloop
+# ÁPDÉT
+# YOLOOOOO
+# Gameloop
 while 1:
     # 5   clear the screen before drawing it again
     screen.fill(0)
@@ -16,11 +17,9 @@ while 1:
     pygame.display.flip()
     # loop through the events
     for event in pygame.event.get():
-
-
+    
         # check if the event is the X button 
-        if event.type==pygame.QUIT:
+        if event.type == pygame.QUIT:
             # if it is quit the game
-            pygame.quit() 
-            exit(0) 
-
+            pygame.quit()
+            exit(0)
