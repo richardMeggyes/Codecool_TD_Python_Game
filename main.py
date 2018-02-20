f58dacdd1312dd3d8a3748055e03b7ebebae2fd8
Mezi <mezitomi@gmail.com>
2018-02-20 14:26:09 +0100

    added sample textures, sample map1, currently working in progress

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 import pygame
 
-=======
+
 import tkinter
 import pygame
 from pygame.locals import *
@@ -9,13 +8,36 @@ from pygame.locals import *
 pygame.init()
 width, height = 640, 480
 screen=pygame.display.set_mode((width, height))
+map1=[]
+infile = open("level.txt", 'r')
+for line in infile:
+    line = line.strip('\n')
+    map1.append(line)
+infile.close()
+
+grass = pygame.image.load("grass.png")
+path = pygame.image.load("path.png")
+error = pygame.image.load("error.png")
+res = 20
+
+pygame.display.flip()
+#print(map1)
+
 
 
 #  Gameloop
 while 1:
     # 5   clear the screen before drawing it again
-    screen.fill(0)
-    
+    #screen.fill(0)
+    for x in range(int(width / res)):
+        for y in range(int(height / res)):
+            if (map1[y][x] == '0'):  # 0 means grass
+                screen.blit(grass, (x * res, y * res))
+            elif (map1[y][x] == '1'):  # 1 means path
+                screen.blit(path, (x * res, y * res))
+            else:
+                screen.blit(error, (x*res, y*res))
+
     # update the screen
     pygame.display.flip()
     # loop through the events
@@ -27,4 +49,4 @@ while 1:
             # if it is quit the game
             pygame.quit() 
             exit(0) 
->>>>>>> 66799d301189927cff725efae210e344f6feef77
+
