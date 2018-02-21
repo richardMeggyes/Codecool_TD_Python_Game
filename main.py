0091e0ed2f4f79afefe177814c15a007983ca75b
Mezi <mezitomi@gmail.com>
2018-02-21 12:52:54 +0100

    Refactored code, relocated some files to Assets folder

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import pygame
-
-
-import tkinter
-import pygame
 from pygame.locals import *
 
-def convert_map(map):
-    map_list = []
+
+def get_path_from_map(map):
     sorted_list = []
 
     # start tile
@@ -16,19 +12,17 @@ def convert_map(map):
                 sorted_list.append([iter2, iter])
                 break
 
-
     finished_parsing = False
-
+    # get tiles in order
     while not finished_parsing:
 
         last_x = int(sorted_list[-1][0])
         last_y = int(sorted_list[-1][1])
 
         try:
-            # érts: Ha az utolsó elem 0. kordinátája +1-en és 1. kordinátáján 1-es van:
+            # Check to right
             if map[last_x + 1][last_y] == '1' and [last_x + 1, last_y] not in sorted_list:
                 sorted_list.append([last_x + 1, last_y])
-                #print(sorted_list[-1], '\n')
             elif map[last_x + 1][last_y] == '3':
                 finished_parsing = True
 
@@ -36,30 +30,27 @@ def convert_map(map):
             pass
 
         try:
-            if map[last_x-1][last_y] == '1'and [last_x - 1, last_y] not in sorted_list:
+            # Check to left
+            if map[last_x - 1][last_y] == '1' and [last_x - 1, last_y] not in sorted_list:
                 sorted_list.append([last_x - 1, last_y])
-                #print(sorted_list[-1], '\n')
             elif map[last_x - 1][last_y] == '3':
                 finished_parsing = True
         except:
             pass
 
-
         try:
-            # érts: Ha az utolsó elem 0. kordinátáján és 1. kordinátája + 1-en 1-es van:
+            # Check to down
             if map[last_x][last_y + 1] == '1' and [last_x, last_y + 1] not in sorted_list:
                 sorted_list.append([last_x, last_y + 1])
-                #print(sorted_list[-1], '\n')
             elif map[last_x][last_y + 1] == '3':
                 finished_parsing = True
         except:
             pass
         try:
-            # érts: Ha az utolsó elem 0. kordinátáján és 1. kordinátája - 1-en 1-es van:
+            # Check to up
             if map[last_x][last_y - 1] == '1' and [last_x, last_y - 1] not in sorted_list:
-                sorted_list.append([last_x, last_y -1])
-                #print(sorted_list[-1], '\n')
-            elif map[last_x][last_y - 1]  == '3':
+                sorted_list.append([last_x, last_y - 1])
+            elif map[last_x][last_y - 1] == '3':
                 finished_parsing = True
         except:
             pass
@@ -72,65 +63,61 @@ def convert_map(map):
                 sorted_list.append([iter2, iter])
                 break
 
-
     return sorted_list
 
 
-
-
-def main():
-    pygame.init()
-    width, height = 640, 480
-    screen=pygame.display.set_mode((width, height))
-    map1=[]
-    infile = open("level.txt", 'r')
+def import_map(filename):
+    return_me = []
+    infile = open(filename, 'r')
     for line in infile:
         line = line.strip('\n')
         newline = []
         for c in line:
             newline.append(c)
-        map1.append(newline)
+        return_me.append(newline)
     infile.close()
+    return return_me
 
 
-    print(convert_map(map1))
-
-
-    grass = pygame.image.load("grass.png")
-    path = pygame.image.load("path.png")
-    start_path = pygame.image.load("start_path.png")
-    end_path = pygame.image.load("end_path.png")
-    error = pygame.image.load("error.png")
-    res = 20
+def main():
+    pygame.init()
+    WIDTH, HEIGHT = 640, 480
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    level1 = import_map("Assets/level1.txt")
+    grass = pygame.image.load("Assets/grass.png")
+    path = pygame.image.load("Assets/path.png")
+    start_path = pygame.image.load("Assets/start_path.png")
+    end_path = pygame.image.load("Assets/end_path.png")
+    error = pygame.image.load("Assets/error.png")
+    resolution = 20
 
     pygame.display.flip()
 
-
     #  Gameloop
     while 1:
-    # clear the screen before drawing it again
+        # clear the screen before drawing it again
         screen.fill(0)
-        tiles_list = convert_map(map1)
+        tiles_list = get_path_from_map(level1)
 
-        for y in range(int(height / res)):
-            for x in range(int(width / res)):
-                screen.blit(grass, (x * res, y * res))
+        for y in range(int(HEIGHT / resolution)):
+            for x in range(int(WIDTH / resolution)):
+                screen.blit(grass, (x * resolution, y * resolution))
         for item in tiles_list:
-            screen.blit(path, (item[1]*res, item[0]*res))
+            screen.blit(path, (item[1] * resolution, item[0] * resolution))
 
-        screen.blit(start_path, (tiles_list[0][0]*res, tiles_list[0][1]*res))
-        screen.blit(end_path, (tiles_list[-1][0]*res, tiles_list[-1][1]*res))
-
+        screen.blit(start_path, (tiles_list[0][0] * resolution, tiles_list[0][1] * resolution))
+        screen.blit(end_path, (tiles_list[-1][0] * resolution, tiles_list[-1][1] * resolution))
 
         # update the screen
         pygame.display.flip()
         # loop through the events
         for event in pygame.event.get():
-        # check if the event is the X button
-            if event.type==pygame.QUIT:
-        # if it is quit the game
-               pygame.quit()
-               exit(0)
+            # check if the event is the X button
+            if event.type == pygame.QUIT:
+                # if it is quit the game
+                pygame.quit()
+                exit(0)
 
 
-main()
+if __name__ == "__main__":
+    main()
